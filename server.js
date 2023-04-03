ce90b5d75f23a3d54a049a89aac0d7e2734b1302
Matúš <19matus98@gmail.com>
2023-04-03 13:33:50 +0200

    adding files

---
+++ server.js
@@ -0,0 +1,140 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const knex = require("knex");
+const bcrypt = require("bcrypt");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    database: "moviedb",
+  },
+});
+
+db.select("*")
+  .from("users")
+  .then((data) => {
+    //   console.log(data);
+  });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .transaction((trx) => {
+            trx
+              .select("*")
+              .from("users")
+              .where("email", "=", req.body.email)
+              .then((user) => {
+                return trx("users")
+                  .where("id", "=", user[0].id)
+                  .increment("entries", 1)
+                  .returning("entries")
+                  .then((entries) => {
+                    res.json({
+                      name: user[0].name,
+                      entries: entries[0],
+                    });
+                  });
+              })
+              .then(trx.commit)
+              .catch(trx.rollback);
+          })
+          .catch((err) =>
+            res.status(400).json("unable to update user entries")
+          );
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+});
+
+app.post("/register", (req, res) => {
+  const saltRounds = 10;
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("incorrect form submission");
+  }
+  const hash = bcrypt.hashSync(password, saltRounds);
+  db.transaction((trx) => {
+    trx
+      .select("*")
+      .from("users")
+      .where({ email })
+      .then((users) => {
+        if (users.length > 0) {
+          res.status(400).json("Email already registered");
+        } else {
+          return trx
+            .insert({
+              hash: hash,
+              email: email,
+            })
+            .into("login")
+            .returning("email")
+            .then((loginEmail) => {
+              return trx("users")
+                .returning("*")
+                .insert({
+                  email: loginEmail[0].email,
+                  name: name,
+                  joined: new Date(),
+                })
+                .then((user) => {
+                  res.json(user);
+                });
+            });
+        }
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+});
+
+app.listen(3001, () => {
+  console.log("app on running on port 3001.");
+});
