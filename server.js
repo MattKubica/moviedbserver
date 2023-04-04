ff26ac723e4d3b964853de570468211567c0bd04
Matúš <19matus98@gmail.com>
2023-04-04 21:24:00 +0200

    poppppp

--- server.js
+++ server.js
@@ -137,6 +137,6 @@ app.put("/image", (req, res) => {
     .catch((err) => res.status(400).json("unable to get entries"));
 });
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log("app on running on port 3001.");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app on running on port  ${PORT}`);
 });
